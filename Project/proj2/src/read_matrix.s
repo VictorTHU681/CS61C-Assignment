e330ca2729b59dacf0a0542fa66a50d8aef1a5ef
CJ <wang-cj23@mails.tsinghua.edu.cn>
2025-08-05 17:49:07 +0800

    Proj2, ReadMatrix is implemented.No exceptions is considered.

--- Project/proj2/src/read_matrix.s
+++ Project/proj2/src/read_matrix.s
@@ -27,16 +27,47 @@
 read_matrix:
 
     # Prologue
+	addi sp, sp, -16
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)
+	sw ra, 12(sp)
+
+	mv s0, a0
+	lw s1 0(a1)
+	lw s2 0(a2) # transfer pointers to the ints
 	
+	# Malloc
+	li t0 4
+	mul t0, t0, s1
+	mul a0, t0, s2
+	mv s2, a0 # s2 is # of bytes to malloc & read in
+	jal malloc
+	mv s1, a0 # s1 is the pointer to malloced memory 
 
+	# Read from file
+	mv a1, s0
+	li a2, 0 # read only 
+	jal fopen
 
+	mv s0, a0 # s0 is the descriptor
+	mv a1, s0
+	mv a2, s1
+	li a3, 8 
+	jal fread # First Read
 
+	mv a1, s0
+	mv a2, s1 # overwirte the previous read result
+	mv a3, s2 
+	jal fread # Second Read
 
-
-
-
+	# Close the file
+	mv a1, s0
 
     # Epilogue
-
-
+	addi sp, sp, 16 
+	lw s0, 0(sp)
+	lw s1, 4(sp)
+	lw s2, 8(sp)
+	lw ra, 12(sp)
     ret
